7b2c1386d53f89dd9ada1ea4d70be0d3c05e9c69
Phillip R <philliprosejob@gmail.com>
2022-07-25 22:21:22 -0600

    ran into bug when trying out the weather API, will need to read docs

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // make sure both the key const has a key inside the string.
 // html elements saved to variables 
 var inputEl = document.querySelector('input');
+// needed html variables list 
+   // weather card class 
+
 
 
 
@@ -11,21 +14,25 @@ const weatherKey = `e4d3b96206444e95a9c7ce98d06c55b0`
 // mapKey is: 28oyI0GbeI2xfeMfXGihR4g2FOlIRb4
 const mapKey = '28oyI0GbeI2xfeMfXGihR4g2FOlIRb4p'
 // use these to hold the Lon and Lat values from the weather api for use in Mapquest api
-var apiLon, apiLat;
+var apiLon; 
+var apiLat;
 // on click for the weather button 
-function getWeather(){
+function weatherBtn(){
    // get input value from user 
    inputValue = inputEl.value;
    // add that value to the URL with correct syntax 
-   var apiUrl = (`https://api.weatherbit.io/v2.0/forecast/daily?postal_code=${inputValue}&days=5&key=${weatherKey}`);
+   var apiUrl = (`https://api.weatherbit.io/v2.0/forecast/daily?postal_code=${inputValue}&days=5&units=I&key=${weatherKey}`);
    // Pass the URL to the fetch function to get the data
    getWeather(apiUrl);   
  
 };
 // on click for the ice cream button takes in the Lon and Lat from weather api. Calls the Mapquest fetch.
-function getStores(lon, lat){
+function storeBtn(lon, lat){
    apiLon = lon;
    apiLat = lat;
+   console.log(apiLon);
+   console.log(apiLat);
+   debugger;
    storeApi = `https://www.mapquestapi.com/search/v4/place?location=${apiLon}%2C%20${apiLat}&sort=distance&feedback=false&key=${mapKey}&pageSize=5&q=ice%20cream`;   
 
    getStores(storeApi);
@@ -34,7 +41,7 @@ function getStores(lon, lat){
 
 
 // WORKING FETCH FOR WEATHER API
- var getWeather = function(apiUrl){
+function getWeather(apiUrl){
     // add any extra variables here 
     // take in above URL to get data 
     var weatherData = fetch(apiUrl)
@@ -48,7 +55,7 @@ function getStores(lon, lat){
      });
   return weatherData;
 };
-// store fetch request from Mapquest api
+// fetch request from Mapquest api
 function getStores(storeApi){
  var shopData =  fetch(storeApi)
     .then(function(response){
