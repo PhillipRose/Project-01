db41e9a7035c02f0542de7290a22acce3d3a48de
Phillip R <philliprosejob@gmail.com>
2022-07-25 21:46:58 -0600

    Formatted URL for custom values, split the on click functions into 2 seperate ones.

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
-// make sure the key const has a key inside the string.
-// html input element saved to variable 
+// make sure both the key const has a key inside the string.
+// html elements saved to variables 
 var inputEl = document.querySelector('input');
+
+
+
+
 // my key if needed, you can use your own just assign it to the key below as a string. 
 // e4d3b96206444e95a9c7ce98d06c55b0
-const key = `e4d3b96206444e95a9c7ce98d06c55b0`
-// this will be how we show to data to the user 
-var pTag = document.querySelector('p');
-
-function clicker(){
+const weatherKey = `e4d3b96206444e95a9c7ce98d06c55b0`
+// mapKey is: 28oyI0GbeI2xfeMfXGihR4g2FOlIRb4
+const mapKey = '28oyI0GbeI2xfeMfXGihR4g2FOlIRb4p'
+// use these to hold the Lon and Lat values from the weather api for use in Mapquest api
+var apiLon, apiLat;
+// on click for the weather button 
+function getWeather(){
    // get input value from user 
    inputValue = inputEl.value;
    // add that value to the URL with correct syntax 
-   var apiUrl = (`https://api.weatherbit.io/v2.0/forecast/daily?postal_code=${inputValue}&days=5&key=${key}`);
+   var apiUrl = (`https://api.weatherbit.io/v2.0/forecast/daily?postal_code=${inputValue}&days=5&key=${weatherKey}`);
    // Pass the URL to the fetch function to get the data
-   storeApi = 'https://www.mapquestapi.com/search/v4/place?location=-111.8581%2C%2040.7372&sort=distance&feedback=true&key=28oyI0GbeI2xfeMfXGihR4g2FOlIRb4p&pageSize=20&q=ice%20cream' 
-   getData(apiUrl);
+   getWeather(apiUrl);   
+ 
+};
+// on click for the ice cream button takes in the Lon and Lat from weather api. Calls the Mapquest fetch.
+function getStores(lon, lat){
+   apiLon = lon;
+   apiLat = lat;
+   storeApi = `https://www.mapquestapi.com/search/v4/place?location=${apiLon}%2C%20${apiLat}&sort=distance&feedback=false&key=${mapKey}&pageSize=5&q=ice%20cream`;   
+
    getStores(storeApi);
+
 }
 
 
 // WORKING FETCH FOR WEATHER API
- var getData = function(apiUrl){
+ var getWeather = function(apiUrl){
     // add any extra variables here 
     // take in above URL to get data 
     var weatherData = fetch(apiUrl)
@@ -30,14 +44,10 @@ function clicker(){
         return response.json();
      })
      .then(function(data){
-      var forecastData = data
-      weatherHolder(forecastData); 
-     return forecastData;     
+      console.log(data);
      });
-     weatherHolder(weatherData);
   return weatherData;
 };
-
 // store fetch request from Mapquest api
 function getStores(storeApi){
  var shopData =  fetch(storeApi)
@@ -48,11 +58,11 @@ function getStores(storeApi){
 
          var shopData = data;
          console.log(data);
-         shopHolder(shopData);
          return shopData;
       })
       return shopData;
 };
+
 
 
 
